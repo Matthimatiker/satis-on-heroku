88b1bdf23e0e3c4c933345936bcdfdc7e1b9b1d1
Matthias Molitor <mmo@webfactory.de>
2016-04-09 15:31:05 +0200

    generate the Satis config from environment variables

--- .profile
+++ .profile
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Generate the Satis config
+php bin/generate-satis-config.php
+
 # Perform an initial build when the instance starts.
 ./vendor/bin/satis build --no-interaction
--- bin/generate-satis-config.php
+++ bin/generate-satis-config.php
@@ -2,8 +2,15 @@
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
+$repositories = array_filter($_SERVER, function ($key) {
+    return strpos($key, 'SATIS_REPOSITORY_') === 0;
+}, ARRAY_FILTER_USE_KEY);
+
 $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../views');
 $twig = new \Twig_Environment($loader);
 
-$config = $twig->render('satis.json.twig', array('env' => $_SERVER));
-file_put_contents(__DIR__ . '/../satis.twig.json', $config);
+$config = $twig->render('satis.json.twig', array(
+    'env' => $_SERVER,
+    'repositories' => $repositories
+));
+file_put_contents(__DIR__ . '/../satis.json', $config);
