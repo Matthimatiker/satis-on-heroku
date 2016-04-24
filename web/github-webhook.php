5bbce93a8c062b87ce9a558535b14862bd7d8345
Matthias Molitor <mmo@webfactory.de>
2016-04-24 17:50:57 +0200

    use symfony/process to start the update process (#3)

--- web/github-webhook.php
+++ web/github-webhook.php
@@ -5,6 +5,8 @@ namespace Matthimatiker\SatisOnHeroku;
 use Composer\Config;
 use Composer\Json\JsonFile;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessUtils;
 
 require(__DIR__ . '/../vendor/autoload.php');
 
@@ -39,9 +41,17 @@ $configuredRepositories = array_filter($configuredRepositories);
 
 $matches = array_intersect($configuredRepositories, $possibleRepositoryUrls);
 if (count($matches) === 0) {
-    echo 'Cannot update, none of the following repositories is managed by this Satis instance: ' . implode(',', $possibleRepositoryUrls);
+    echo 'Cannot update, none of the following repositories is managed by this Satis instance: ' . implode(', ', $possibleRepositoryUrls);
     exit();
 }
 $repositoryToUpdate = current($matches);
 
-var_dump($repositoryToUpdate);
+$command = 'vendor/bin/satis --repository-url=' . ProcessUtils::escapeArgument($repositoryToUpdate);
+$process = new Process($command, __DIR__ . '/..');
+$process->run(function ($type, $buffer) {
+    if (Process::ERR === $type) {
+        echo 'ERR > '.$buffer;
+    } else {
+        echo 'OUT > '.$buffer;
+    }
+});
