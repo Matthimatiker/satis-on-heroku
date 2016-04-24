ce5bd16733169653512385db7ea1e104af61b054
Matthias Molitor <mmo@webfactory.de>
2016-04-24 17:28:59 +0200

    read repositories from Satis config (#3)

--- web/github-webhook.php
+++ web/github-webhook.php
@@ -2,6 +2,8 @@
 
 namespace Matthimatiker\SatisOnHeroku;
 
+use Composer\Config;
+use Composer\Json\JsonFile;
 use Symfony\Component\HttpFoundation\Request;
 
 require(__DIR__ . '/../vendor/autoload.php');
@@ -18,5 +20,14 @@ if (!isset($payload['repository']['full_name'])) {
     echo 'Expected repository name in payload.';
     exit();
 }
+$repositoryName = $payload['repository']['full_name'];
+$possibleRepositoryUrls = array(
+    sprintf('git@github.com:%s.git', $repositoryName),
+    sprintf('https://github.com/%s.git', $repositoryName),
+);
 
-var_dump($payload);
+$satisConfigFile = new JsonFile(__DIR__ . '/../satis.json');
+$config = new Config();
+$config->merge(array('config' => $satisConfigFile->read()));
+
+var_dump($possibleRepositoryUrls, $config->getRepositories());
