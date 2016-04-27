2e003ac32b66ce213cf6114571826913ed5e163d
Matthias Molitor <mmo@webfactory.de>
2016-04-27 11:24:35 +0200

    added example URLs (#5)

--- tests/RepositoryUrlTest.php
+++ tests/RepositoryUrlTest.php
@@ -4,6 +4,12 @@ namespace Matthimatiker\SatisOnHeroku\Tests;
 
 class RepositoryUrlTest extends \PHPUnit_Framework_TestCase
 {
+    const URL_GITHUB_SSH  = 'git@github.com:Matthimatiker/satis-on-heroku.git';
+    const URL_GITHUB_HTTP = 'https://github.com/Matthimatiker/satis-on-heroku.git';
+    const URL_KILN_SSH    = 'ssh://user@my-company.kilnhg.com/project/group/repo';
+    const URL_KILN_HTTP   = 'https://my-company.kilnhg.com/Code/project/group/repo.git';
+    const URL_PACKAGIST   = 'https?://packagist.org';
+
     public function testToStringReturnsOriginalGitHubSshUrl()
     {
 
