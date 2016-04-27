3eb1a8218cccf031284bba2d0b3b2366a26637fc
Matthias Molitor <mmo@webfactory.de>
2016-04-27 11:42:12 +0200

    implemented tests (#5)

--- tests/RepositoryUrlTest.php
+++ tests/RepositoryUrlTest.php
@@ -2,6 +2,8 @@
 
 namespace Matthimatiker\SatisOnHeroku\Tests;
 
+use Matthimatiker\SatisOnHeroku\RepositoryUrl;
+
 class RepositoryUrlTest extends \PHPUnit_Framework_TestCase
 {
     const URL_GITHUB_SSH  = 'git@github.com:Matthimatiker/satis-on-heroku.git';
@@ -12,41 +14,60 @@ class RepositoryUrlTest extends \PHPUnit_Framework_TestCase
 
     public function testToStringReturnsOriginalGitHubSshUrl()
     {
-
+        $this->assertEquals(self::URL_GITHUB_SSH, (string)$this->createFrom(self::URL_GITHUB_SSH));
     }
 
     public function testToStringReturnsOriginalGitHubHttpUrl()
     {
-
+        $this->assertEquals(self::URL_GITHUB_HTTP, (string)$this->createFrom(self::URL_GITHUB_HTTP));
     }
 
     public function testToStringReturnsOriginalKilnSshUrl()
     {
-
+        $this->assertEquals(self::URL_KILN_SSH, (string)$this->createFrom(self::URL_KILN_SSH));
     }
 
     public function testToStringReturnsOriginalKilnHttpUrl()
     {
+        $this->assertEquals(self::URL_KILN_HTTP, (string)$this->createFrom(self::URL_KILN_HTTP));
+    }
 
+    public function testToStringReturnsOriginalPackagistUrl()
+    {
+        $this->assertEquals(self::URL_PACKAGIST, (string)$this->createFrom(self::URL_PACKAGIST));
     }
 
     public function testGetHostReturnsCorrectValueForGitHubSshUrl()
     {
-
+        $this->assertEquals('github.com', $this->createFrom(self::URL_GITHUB_SSH)->getHost());
     }
 
     public function testGetHostReturnsCorrectValueForGitHubHttpUrl()
     {
-
+        $this->assertEquals('github.com', $this->createFrom(self::URL_GITHUB_HTTP)->getHost());
     }
 
     public function testGetHostReturnsCorrectValueForKilnSshUrl()
     {
-
+        $this->assertEquals('my-company.kilnhg.com', $this->createFrom(self::URL_KILN_SSH)->getHost());
     }
 
     public function testGetHostReturnsCorrectValueForKilnHttpUrl()
     {
+        $this->assertEquals('my-company.kilnhg.com', $this->createFrom(self::URL_KILN_HTTP)->getHost());
+    }
 
+    public function testGetHostReturnsCorrectValueForPackagistUrl()
+    {
+        $this->assertEquals('packagist.org', $this->createFrom(self::URL_PACKAGIST)->getHost());
+    }
+
+    /**
+     * @param string $url
+     * @return RepositoryUrl
+     */
+    private function createFrom($url)
+    {
+        return new RepositoryUrl($url);
     }
 }
