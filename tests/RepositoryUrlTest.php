8470c93934c0e8447d564cc4d0424fb5a8e5754c
Matthias Molitor <mmo@webfactory.de>
2016-04-27 11:04:09 +0200

    added test case stub (#5)

---
+++ tests/RepositoryUrlTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Matthimatiker\SatisOnHeroku\Tests;
+
+class RepositoryUrlTest extends \PHPUnit_Framework_TestCase
+{
+    public function testToStringReturnsOriginalGitHubSshUrl()
+    {
+
+    }
+
+    public function testToStringReturnsOriginalGitHubHttpUrl()
+    {
+
+    }
+
+    public function testToStringReturnsOriginalKilnSshUrl()
+    {
+
+    }
+
+    public function testToStringReturnsOriginalKilnHttpUrl()
+    {
+
+    }
+
+    public function testGetHostReturnsCorrectValueForGitHubSshUrl()
+    {
+
+    }
+
+    public function testGetHostReturnsCorrectValueForGitHubHttpUrl()
+    {
+
+    }
+
+    public function testGetHostReturnsCorrectValueForKilnSshUrl()
+    {
+
+    }
+
+    public function testGetHostReturnsCorrectValueForKilnHttpUrl()
+    {
+
+    }
+}
