f53d213d73837d31baf9662b9cc48ed4195e8186
Matthias Molitor <mmo@webfactory.de>
2016-04-27 10:55:56 +0200

    added stub for repository URL class (#5)

---
+++ src/RepositoryUrl.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Matthimatiker\SatisOnHeroku;
+
+/**
+ * Represents a repository URL.
+ */
+class RepositoryUrl
+{
+    /**
+     * @param string $url
+     */
+    public function __construct($url)
+    {
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+
+    }
+
+    /**
+     * @return string The original URL.
+     */
+    public function __toString()
+    {
+
+    }
+}
