9ca2613a246a1e90318b4e5abf4f6492f3ecde34
Matthias Molitor <mmo@webfactory.de>
2016-04-28 19:02:30 +0200

    added test stubs (#5)

--- tests/SatisConfigTest.php
+++ tests/SatisConfigTest.php
@@ -4,5 +4,28 @@ namespace Matthimatiker\SatisOnHeroku\Tests;
 
 class SatisConfigTest extends \PHPUnit_Framework_TestCase
 {
+    public function testGetRepositoryUrlsReturnsCorrectNumberOfUrls()
+    {
 
+    }
+
+    public function testGetRepositoryUrlsReturnsUrlObjects()
+    {
+
+    }
+
+    public function testGetRepositoryHostsReturnsCorrectHosts()
+    {
+
+    }
+
+    public function testGetRepositoryHostsDoesNotReturnSameHostTwice()
+    {
+
+    }
+
+    public function testThrowsExceptionIfConfigFileDoesNotExist()
+    {
+
+    }
 }
