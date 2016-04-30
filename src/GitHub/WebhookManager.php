a227737a06189066b2360b9c6fd3a4f2887d9fc7
Matthias Molitor <mmo@webfactory.de>
2016-04-30 23:20:28 +0200

    implemented hook manager (#5)

--- src/GitHub/WebhookManager.php
+++ src/GitHub/WebhookManager.php
@@ -12,11 +12,25 @@ use Matthimatiker\SatisOnHeroku\RepositoryUrl;
 class WebhookManager
 {
     /**
-     * @param Hooks $hookApi
+     * @var Hooks
+     */
+    protected $hooksApi = null;
+
+    /**
+     * The URL to the webhook.
+     *
+     * @var Url
+     */
+    protected $hookUrl = null;
+
+    /**
+     * @param Hooks $hooksApi
      * @param Url $webhookUrl
      */
-    public function __construct(Hooks $hookApi, Url $webhookUrl)
+    public function __construct(Hooks $hooksApi, Url $webhookUrl)
     {
+        $this->hooksApi = $hooksApi;
+        $this->hookUrl = $webhookUrl;
     }
 
     /**
@@ -25,7 +39,8 @@ class WebhookManager
      */
     public function supports(RepositoryUrl $url)
     {
-
+        list($user, $repository) = $this->getUserAndRepositoryFrom($url);
+        return $user !== null && $repository !== null;
     }
 
     /**
@@ -33,6 +48,74 @@ class WebhookManager
      */
     public function registerFor(RepositoryUrl $url)
     {
+        $activeHook = $this->findActiveSatisHook($url);
+        if ($activeHook === null) {
+            // Hook does not exist, create one.
+            list($user, $repository) = $this->getUserAndRepositoryFrom($url);
+            $this->hooksApi->create($user, $repository, $this->getHookDefinition());
+            return;
+        }
+        if ((string)$this->hookUrl === $activeHook['config']['url']) {
+            // Hook URL did not change, no update necessary.
+            return;
+        }
+        list($user, $repository) = $this->getUserAndRepositoryFrom($url);
+        $this->hooksApi->update($user, $repository, $activeHook['id'], $this->getHookDefinition());
+    }
 
+    /**
+     * Returns the hook definition that can be used with the GitHub API.
+     *
+     * @return array<string=>mixed>
+     */
+    private function getHookDefinition()
+    {
+        return array(
+            'name' => 'web',
+            'config' => array(
+                'url' => (string)$this->hookUrl,
+                'content_type' => 'json',
+                'insecure_ssl' => '0'
+            )
+        );
+    }
+
+    /**
+     * @param RepositoryUrl $url
+     * @return array<string, mixed>|null
+     */
+    private function findActiveSatisHook(RepositoryUrl $url)
+    {
+        list($user, $repository) = $this->getUserAndRepositoryFrom($url);
+        $alreadyRegisteredHooks = $this->hooksApi->all($user, $repository);
+        foreach ($alreadyRegisteredHooks as $definition) {
+            /* @var $definition array<string, mixed> */
+            if ($this->hookUrl->getHost() === parse_url($definition['config']['url'], PHP_URL_HOST)) {
+                return $definition;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Example:
+     *
+     *     list($user, $repository = $this->getUserAndRepositoryFrom($url);
+     *
+     * @param RepositoryUrl $url
+     * @return array<string|null>
+     */
+    private function getUserAndRepositoryFrom(RepositoryUrl $url)
+    {
+        if ($url->getHost() !== 'github.com') {
+            return array(null, null);
+        }
+        $segments = $url->getPathSegments();
+        if (count($segments) !== 2) {
+            return array(null, null);
+        }
+        list($owner, $repository) = $segments;
+        $repository = basename($repository, '.git');
+        return array($owner, $repository);
     }
 }
